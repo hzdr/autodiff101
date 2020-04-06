2d8080f6fd329771e8998bd44ca3d872f1aeca73
Steve Schmerler <s.schmerler@hzdr.de>
2020-04-07 00:42:46 +0200

    test_ag: add mysum(), explain why not used

--- test_ag.py
+++ test_ag.py
@@ -35,9 +35,14 @@ def mysin(x):
     return np.sin(x)
 
 
+@primitive
+def mysum(x):
+    return np.sum(x)
+
+
 def pow2_vjp(ans, x):
     # correct for x scalar or (n,), use this in production code
-    return lambda g: 2*g*x
+    return lambda g: g * 2*x
 
 
 def pow2_vjp_with_jac(ans, x):
@@ -47,12 +52,12 @@ def pow2_vjp_with_jac(ans, x):
     # called from grad(func)(x) with x scalar, here we get passed np.array(x)
     # which has shape ()
     if x.shape == ():
-        return lambda g: 2*g*x
+        return lambda g: g * 2*x
     # called from elementwise_grad(func)(x) with x 1d array
     else:
         # The same:
-        #   jac = jacobian(lambda x: np.power(x,2))(x)
-        jac = np.diag(2*x)
+        #   jac = np.diag(2*x)
+        jac = jacobian(lambda x: np.power(x,2))(x)
         return lambda g: np.dot(g, jac)
 
 
@@ -60,17 +65,24 @@ def mysin_vjp(ans, x):
     return lambda g: g * np.cos(x)
 
 
+# Works in JAX but not here in the elementwise_grad() test. See
+# autograd/numpy/numpy_vjps.py -> grad_np_sum() for ow they do it. The returned
+# g's shape must be corrected sometimes.
+##def mysum_vjp(ans, x):
+##    return lambda g: g
+
+
 def func(x):
     return np.sum(np.power(np.sin(x),2))
+
 
 def func_with_vjp(x):
     return np.sum(pow2(mysin(x)))
 
 
+defvjp(mysin, mysin_vjp)
 for p_jvp in [pow2_vjp, pow2_vjp_with_jac]:
     defvjp(pow2, p_jvp)
-    defvjp(mysin, mysin_vjp)
-
 
     assert np.allclose([func(xi)          for xi in x],
                        [func_with_vjp(xi) for xi in x])
