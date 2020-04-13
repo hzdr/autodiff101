b57ee89ca7f42d9d99b5979b049b2f3dc8f8ab04
Steve Schmerler <s.schmerler@hzdr.de>
2020-04-14 00:39:14 +0200

    test_ag: cleanup, enable mysum_vjp

--- test_ag.py
+++ test_ag.py
@@ -5,41 +5,15 @@ import numpy as np
 
 rand = np.random.rand
 
-# scalar derivative
-# df/dx : R -> R
-assert wnp.allclose(grad(wnp.sin)(1.234), wnp.cos(1.234))
-
-# The Jacobian of a vectorized numpy function is diagonal and square $J \in
-# R^{n\times n}$ with
-# $J_{ii} = df_i/dx_i$ where $f_i \equiv f \forall i$
-# $J : R^n -> R^n$
-x = rand(10)*5 - 5
-assert wnp.allclose(jacobian(wnp.sin)(x), wnp.diag(wnp.cos(x)))
-
-# `elementwise_grad` df/dx : R^n -> R^n (vectorized), returns the column sum of
-# the Jacobian
-assert wnp.allclose(elementwise_grad(wnp.sin)(x), wnp.cos(x))
-assert wnp.allclose(jacobian(wnp.sin)(x).sum(axis=0), wnp.cos(x))
-
 
 @primitive
 def pow2(x):
     return x**2
 
 
-@primitive
-def mysin(x):
-    return wnp.sin(x)
-
-
-@primitive
-def mysum(x):
-    return wnp.sum(x)
-
-
 def pow2_vjp(ans, x):
     # correct for x scalar or (n,), use this in production code
-    return lambda g: g * 2*x
+    return lambda v: v * 2*x
 
 
 def pow2_vjp_with_jac(ans, x):
@@ -47,26 +21,43 @@ def pow2_vjp_with_jac(ans, x):
     necessary in practice, only for demonstration.
     """
     # called from grad(func)(x) with x scalar, here we get passed np.array(x)
-    # which has shape ()
+    # with x.shape == ()
     if x.shape == ():
-        return lambda g: g * 2*x
-    # called from elementwise_grad(func)(x) with x 1d array
+        return lambda v: v * 2*x
+    # called from grad(func)(x) and elementwise_grad(func)(x) with x.shape ==
+    # (n,)
     else:
         # The same:
         #   jac = np.diag(2*x)
         jac = jacobian(lambda x: wnp.power(x,2))(x)
-        return lambda g: wnp.dot(g, jac)
+        return lambda v: wnp.dot(v, jac)
+
+
+@primitive
+def mysin(x):
+    return wnp.sin(x)
 
 
 def mysin_vjp(ans, x):
-    return lambda g: g * wnp.cos(x)
+    return lambda v: v * wnp.cos(x)
 
 
-# Works in JAX but not here in the elementwise_grad() test. See
-# autograd/numpy/numpy_vjps.py -> grad_np_sum() for ow they do it. The returned
-# g's shape must be corrected sometimes.
-##def mysum_vjp(ans, x):
-##    return lambda g: g
+@primitive
+def mysum(x):
+    return wnp.sum(x)
+
+
+def mysum_vjp(ans, x):
+    """
+    See autograd/numpy/numpy_vjps.py -> grad_np_sum() for how they do it. The
+    returned v's shape must be corrected sometimes. Here we explicitly write
+    out the JVP using jacobian(wnp.sum)(x) == wnp.ones_like(x) which is always
+    correct. See test_jax.py:mysum_jvp() for more comments. Note that in
+    contrast to JAX, here v is *always* scalar, a fact that we can't explain
+    ATM. As JAX is autograd 2.0, we consider this an autograd quirk and leave
+    it be.
+    """
+    return lambda v: wnp.dot(v, wnp.ones_like(x))
 
 
 def func(x):
@@ -74,21 +65,43 @@ def func(x):
 
 
 def func_with_vjp(x):
-    return wnp.sum(pow2(mysin(x)))
+    return mysum(pow2(mysin(x)))
 
 
-defvjp(mysin, mysin_vjp)
-for p_jvp in [pow2_vjp, pow2_vjp_with_jac]:
-    defvjp(pow2, p_jvp)
+def test():
+    # scalar derivative
+    # df/dx : R -> R
+    assert wnp.allclose(grad(wnp.sin)(1.234), wnp.cos(1.234))
 
-    assert wnp.allclose([func(xi)          for xi in x],
-                        [func_with_vjp(xi) for xi in x])
+    x = rand(10)*5 - 5
+    assert wnp.allclose(jacobian(wnp.sin)(x), wnp.diag(wnp.cos(x)))
 
-    assert wnp.allclose(func(x),
-                        func_with_vjp(x))
+    # elementwise_grad(f) : R^n -> R^n (of f: R^n -> R^n), returns the column sum of
+    # the Jacobian
+    assert wnp.allclose(elementwise_grad(wnp.sin)(x), wnp.cos(x))
+    assert wnp.allclose(jacobian(wnp.sin)(x).sum(axis=0), wnp.cos(x))
 
-    assert wnp.allclose([grad(func)(xi)          for xi in x],
-                        [grad(func_with_vjp)(xi) for xi in x])
 
-    assert wnp.allclose(elementwise_grad(func)(x),
-                        elementwise_grad(func_with_vjp)(x))
+    defvjp(mysin, mysin_vjp)
+    defvjp(mysum, mysum_vjp)
+    for p2_jvp in [pow2_vjp, pow2_vjp_with_jac]:
+        defvjp(pow2, p2_jvp)
+
+        assert wnp.allclose([func(xi)          for xi in x],
+                            [func_with_vjp(xi) for xi in x])
+
+        assert wnp.allclose(func(x),
+                            func_with_vjp(x))
+
+        assert wnp.allclose([grad(func)(xi)          for xi in x],
+                            [grad(func_with_vjp)(xi) for xi in x])
+
+        assert wnp.allclose(elementwise_grad(func)(x),
+                            elementwise_grad(func_with_vjp)(x))
+
+        assert wnp.allclose(grad(func)(x),
+                            grad(func_with_vjp)(x))
+
+
+if __name__ == '__main__':
+    test()
